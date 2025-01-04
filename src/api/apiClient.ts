d681876464c66f74b1d76864bc55c00c28048659
Lee Yuna <leeyuna7689@gmail.com>
2025-01-04 18:04:18 +0900

    feat: axios 인터셉터를 활용한 인증 처리 추가
    - axios 인스턴스 생성
    - 헤더에 액세스 토큰을 자동으로 넣어주는 인터셉터 추가
    - 요청 실패 시, 토큰 재발급 및 요청 재시도를 처리하는 인터셉터 추가

---
+++ src/api/apiClient.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from "axios";
+import { redirect } from "next/navigation";
+
+import ResponseData from "@/app/models/response-data";
+import useAccessTokenStore from "@/store/accessToken";
+import { reissueAccessToken } from "@/utils/auth";
+
+// 헤더에 액세스 토큰이 필요한 요청에 사용
+const authApiClient = axios.create({
+  baseURL: "http://localhost:8080/",
+});
+
+authApiClient.interceptors.request.use(async (config) => {
+  const accessToken = useAccessTokenStore.getState().accessToken;
+  config.headers["access"] = accessToken;
+  return config;
+});
+
+authApiClient.interceptors.response.use(
+  async (response: AxiosResponse<ResponseData>) => {
+    const originalRequest = response.config;
+
+    if (response.data.code === 401 && !originalRequest._retry) {
+      await reissueAccessToken();
+      originalRequest._retry = true;
+      authApiClient(originalRequest);
+    } else if (response.data.code === 400) {
+      redirect("/");
+    }
+
+    return response;
+  }
+);
