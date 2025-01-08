5d722b296502c69e63a1da30b8e05853c115b68f
Lee Yuna <leeyuna7689@gmail.com>
2025-01-08 19:19:35 +0900

    feat: 닉네임 페이지 추가

--- src/app/nickname/page.tsx
+++ src/app/nickname/page.tsx
@@ -1,3 +1,16 @@
+import Header from "@/components/header/header";
+import NicknameForm from "@/components/nickname/nickname-form";
+
 export default async function NicknamePage() {
-  return <div>Nickname Setting</div>;
+  return (
+    <>
+      <Header>
+        <div>LOGO</div>
+      </Header>
+      <main className="flex flex-col gap-16 px-5 py-8 bg-white">
+        <h1 className="text-xl font-bold">이름을 입력해주세요</h1>
+        <NicknameForm />
+      </main>
+    </>
+  );
 }
