ccc0b639c72446982d7520ffcfb85e61bc98a31c
Lee Yuna <leeyuna7689@gmail.com>
2025-01-10 16:22:18 +0900

    feat: Axios 인스턴스 apiClient 추가

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -5,9 +5,13 @@ import ResponseData from "@/models/response-data";
 import useAccessTokenStore from "@/store/accessToken";
 import { reissueAccessToken } from "@/api/auth";
 
+export const apiClient = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
 // 헤더에 액세스 토큰이 필요한 요청에 사용
 export const authApiClient = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "http://localhost:8080",
 });
 
 authApiClient.interceptors.request.use((config) => {
