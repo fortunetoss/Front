3897e17dc45de54f29f683030a7cf79a79bf616f
Lee Yuna <leeyuna7689@gmail.com>
2025-01-16 18:11:15 +0900

    design: 로그인 페이지 스타일링

--- src/components/login/kako-login-button.tsx
+++ src/components/login/kako-login-button.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Image from "next/image";
+import kakaoLogo from "@/assets/icons/kakao.svg";
+
 const KAKAO_LOGIN_URL = `${process.env.NEXT_PUBLIC_SERVER_URL}/oauth2/authorization/kakao`;
 
 export default function KakaoLoginButton() {
@@ -7,5 +10,13 @@ export default function KakaoLoginButton() {
     window.location.href = KAKAO_LOGIN_URL;
   };
 
-  return <button onClick={handleClick}>카카오톡 로그인</button>;
+  return (
+    <button
+      onClick={handleClick}
+      className="flex justify-center gap-[7px] w-full bg-[#FEE500] py-[13.5px] px-6 rounded-xl font-semibold"
+    >
+      <Image src={kakaoLogo} alt="카카오톡 로고" width={24} height={24} />
+      카카오톡 로그인
+    </button>
+  );
 }
