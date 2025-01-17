a5fd22ecfd17e981a9da573dcb163be02198f24b
hihilong <hyelong20630@gmail.com>
2025-01-18 06:59:24 +0900

    style : 다른 문제 내러가기 -> 홈으로 이동 멘트수정

--- src/app/pockets/shared/page.tsx
+++ src/app/pockets/shared/page.tsx
@@ -18,7 +18,7 @@ const Shared = () => {
     return (
         <div className="container flex flex-col mx-auto p-4">
             <Notice text="복주머니가 공유되었어요!"></Notice>
-            <p className="mt-10 mb-10 text-9xl justify-center mx-auto">
+            <p className="mt-10 mb-20 text-9xl justify-center mx-auto">
                 🥳
             </p>
 
@@ -31,7 +31,7 @@ const Shared = () => {
                     onClick={handleMovePockets}
                     className="px-6 py-3 text-lg font-medium text-blue border-2 border-blue rounded-lg hover:bg-blue  hover:text-white transition"
                 >
-                    다른 문제 내러가기
+                    홈으로 이동
                 </button>
                 {/*
                 <button
