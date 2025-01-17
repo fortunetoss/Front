19d5b2113af0ae2085e78f70b410389f214b6049
Lee Yuna <leeyuna7689@gmail.com>
2025-01-17 23:24:59 +0900

    design: 모달 경고 문구 및 레이아웃 수정

--- src/components/modal/delete-account-modal.tsx
+++ src/components/modal/delete-account-modal.tsx
@@ -28,7 +28,12 @@ export default function DeleteAccountModal({
         className="w-full text-[17px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-[18px] px-5 py-[15px] bg-white z-20 rounded-xl"
         open
       >
-        <p className="">정말 탈퇴하시겠습니까?</p>
+        <section className="flex flex-col gap-[10px]">
+          <h1 className="font-semibold">탈퇴하시겠습니까?</h1>
+          <p className="text-sm">
+            탈퇴 버튼 선택 시, 계정은 삭제되며 복구되지 않습니다.
+          </p>
+        </section>
         <section className="flex justify-end gap-[30px] text-blue">
           <button className="" onClick={onClose}>
             취소
