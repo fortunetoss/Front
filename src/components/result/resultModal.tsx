6358376732eb15d407ecb53d4ee3fa88816c02c8
hihilong <hyelong20630@gmail.com>
2025-01-19 16:56:47 +0900

    fix : static flag 오류 해결

--- src/components/result/resultModal.tsx
+++ src/components/result/resultModal.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react";
 import {useState} from "react";
 import useResultStore from "@/app/store/useResultStore";
+import SolverList from "@/components/result/SolverList";
 
 
 interface ResultModalProps{
@@ -10,43 +11,15 @@ interface ResultModalProps{
 }
 
 const ResultModal: React.FC<ResultModalProps> = ({isOpen, onClose}) => {
-    if(!isOpen) return null;
     const [activeTab, setActiveTab] = useState<"correct" | "incorrect">("correct");
     const [loading, setLoading] = useState<boolean>(false); // 로딩 상태
     const [error, setError] = useState<string | null>(null); // 에러 상태
     const { RightSolvers, WrongSolvers } = useResultStore();
 
+    if(!isOpen) return null;
 
+    const solvers = activeTab === "correct" ? RightSolvers : WrongSolvers;
 
-    const SolversList=() => {
-        let solvers;
-        if (loading) {
-            return <p className="text-center text-gray-500">로딩 중...</p>;
-        }
-        if (activeTab === "correct") {
-            solvers = RightSolvers;
-            // zustand 에서 가져온거
-        } else {
-            solvers = WrongSolvers;
-        }
-
-        if (solvers.length === 0) {
-            return (
-                <p className="text-gray-500 text-xl text-center">아직 응답한 사람이 없어요! </p>
-            );
-        }
-        // 만약 정답자 오답자 한명도 없다면 아직 응답한 사람이 한명도 없다는 거니까 응답한 사람이 없다고 반환하기
-        return (
-            <ul className="space-y-3">
-                {solvers.map((solver, index) => (
-                    <li key={index} className="flex items-center space-x-2 text-xl">
-                        <span className="font-semibold">{solver.solver}</span>
-                        <span className="font-semibold">{solver.answer}</span>
-                    </li>
-                ))}
-            </ul>
-        );
-    };
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end  z-50">
@@ -79,7 +52,9 @@ const ResultModal: React.FC<ResultModalProps> = ({isOpen, onClose}) => {
                 <p className="text-m text-gray-500">
                     총 {activeTab === "correct" ? RightSolvers.length : WrongSolvers.length}명
                 </p>
-                <div className="overflow-y-auto max-h-60">{SolversList()}</div>
+                <div className="overflow-y-auto max-h-60">
+                    <SolverList solvers={solvers} loading={false}/>
+                </div>
 
             </div>
         </div>
