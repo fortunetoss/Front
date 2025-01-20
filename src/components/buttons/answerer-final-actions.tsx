b59ae949a68489bc743351063667e8641be603ad
Lee Yuna <leeyuna7689@gmail.com>
2025-01-21 00:56:14 +0900

    feat: 카카오톡 공유 기능 추가
    - 카카오톡 공유 기능 추가
    - 모달 스타일링 수정
    - 응답자 요청에서 authApiClient를 apiClient로 변경

--- src/components/buttons/answerer-final-actions.tsx
+++ src/components/buttons/answerer-final-actions.tsx
@@ -1,12 +1,42 @@
+"use client";
+
 import Link from "next/link";
+import ShareModal from "../modal/share-modal";
+import { useState } from "react";
+import { kakaotalkShare } from "@/utils/share/kakaotalk-share";
 
 export default function AnswererFinalActions() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleCopyUrl = () => {};
+
+  const onKakaoShare = () => {
+    // 경로에 응답 id 뒤에 붙이기
+    kakaotalkShare(`${process.env.NEXT_PUBLIC_URL}/share-result/`, true);
+  };
+
   return (
     <section className="flex flex-col gap-[14px]">
       <Link href="/" className="broad-btn">
         나도 문제 내러가기
       </Link>
-      <button className="broad-btn">내 결과 공유하기</button>
+      <button className="broad-btn" onClick={handleOpenModal}>
+        내 결과 공유하기
+      </button>
+      <ShareModal
+        isOpen={isOpen}
+        onClose={handleCloseModal}
+        onCopyUrl={handleCopyUrl}
+        onKakaoShare={onKakaoShare}
+      />
     </section>
   );
 }
--- src/components/modal/share-modal.tsx
+++ src/components/modal/share-modal.tsx
@@ -7,14 +7,14 @@ import Image from "next/image";
 import { FaLink } from "react-icons/fa";
 import kakaoLogo from "@/assets/kakao-logo-yellow.svg";
 
-interface ModalProps {
+interface ShareModalProps {
   isOpen: boolean;
   onClose: () => void;
   onCopyUrl: () => void;
   onKakaoShare: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({
+const ShareModal: React.FC<ShareModalProps> = ({
   isOpen,
   onClose,
   onCopyUrl,
@@ -23,25 +23,28 @@ const Modal: React.FC<ModalProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-end justify-center z-50">
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-end justify-center z-50"
+      onClick={onClose}
+    >
       {/* 모달 컨테이너 */}
-      <div className="bg-white rounded-2xl px-6 py-4 w-11/12 max-w-3xl shadow-lg mb-10">
+      <div className="bg-white rounded-2xl px-4 py-8 w-11/12 max-w-sm shadow-lg mb-10">
         {/* 헤더 */}
-        <div className="relative flex items-center mb-10">
+        <div className="relative flex items-center mb-5">
           <h3 className="text-xl font-bold text-gray-800 mx-auto">공유하기</h3>
           <button
             onClick={onClose}
-            className="text-gray-500 hover:text-gray-700 text-2xl"
+            className="text-gray-500 hover:text-gray-700 text-2xl "
           >
             ✕
           </button>
         </div>
 
         {/* 컨텐츠 */}
-        <div className="space-y-4">
+        <div className="flex flex-col">
           {/* 카카오톡 공유 */}
           <button
-            className="flex gap-[14px] font-medium text-lg"
+            className="flex gap-[14px] items-center font-medium text-lg w-full px-4 py-3 rounded-lg hover:bg-gray-200"
             onClick={onKakaoShare}
           >
             <Image src={kakaoLogo} alt="카카오톡 로고" width={32} height={32} />
@@ -49,19 +52,19 @@ const Modal: React.FC<ModalProps> = ({
           </button>
 
           {/* 링크 복사 */}
-          <div className="pb-5 ">
-            <button
-              className="flex items-center w-full px-4 py-3  rounded-lg hover:bg-gray-200"
-              onClick={onCopyUrl}
-            >
-              <FaLink className="text-gray-600 mr-3 text-2xl" />
-              <span className="text-gray-600 font-semibold">링크 복사</span>
-            </button>
-          </div>
+          <button
+            className="flex gap-[14px] items-center w-full px-4 py-3 rounded-lg hover:bg-gray-200"
+            onClick={onCopyUrl}
+          >
+            <div className="w-8 h-8 flex justify-center items-center">
+              <FaLink className="text-gray-600 w-5 h-5" />
+            </div>
+            <span className="font-medium text-lg">링크 복사</span>
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-export default Modal;
+export default ShareModal;
