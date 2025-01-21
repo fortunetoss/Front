532d4fe47b5ac957faec8f0c1018d57109c9cdcc
hihilong <hyelong20630@gmail.com>
2025-01-22 04:51:36 +0900

    fix : 페이지로드 연습로직 삭제

--- src/app/pockets/page.tsx
+++ src/app/pockets/page.tsx
@@ -126,16 +126,7 @@ const Pockets = () => {
             ))}
           </div>
 
-          {!isLastPage && (
-              <div className="flex justify-center mt-6">
-                <button
-                    onClick={handleLoadMore}
-                    className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-                >
-                  더 보기
-                </button>
-              </div>
-          )}
+
         </div>
       </div>
   );
