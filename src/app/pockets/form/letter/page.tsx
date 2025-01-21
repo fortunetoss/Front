66d976ef02f95dd2be9f8931eb044d863d0dbcf1
hihilong <hyelong20630@gmail.com>
2025-01-22 04:52:09 +0900

    feat : 덕담 뒤로가기로직 추가

--- src/app/pockets/form/letter/page.tsx
+++ src/app/pockets/form/letter/page.tsx
@@ -10,9 +10,15 @@ import { cardData } from "../../../../utils/images/cardNames";
 import { submitCustomQuestion } from "../../../../api/api-form";
 import Header from "@/components/header/header";
 import BackButton from "@/components/header/back-button";
+import useModifiedStore from "@/app/store/modifiedStore";
+import {postEdit} from "@/api/api-postEdit";
+import {buttonBackClick} from "@/components/edit/buttonBackClick";
 
 const Letter = () => {
     const router = useRouter();
+    const questionCustomId = usePocketStore((state) => state.questionCustomId);
+    // 상태에서 복주머니 ID 가져오기
+
     const {
         title,
         answers,
@@ -27,22 +33,32 @@ const Letter = () => {
 
     const [selectedCard, setSelectedCard] = useState<string>("A"); // 선택된 카드 이름 저장
 
-    // 카드 선택 핸들러
+    //수정사항
+    const { isModified, setModified } = useModifiedStore();
+
+    //카드 선택
     const handleSelectedCard = (name: string) => {
         setSelectedCard(name); // 선택된 카드 이름 업데이트
+        setCard(name);
+        setModified(true);
+        //카드재선택->상태수정됨
 
     };
 
 
 
-    // 덕담 입력 핸들러
+    //덕담 입력
     const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setContent(e.target.value);
         console.log(content);
+        setModified(true);
+        //덕담입력재수정-> 상태수정됨
+
     };
 
-    // 다음 버튼 핸들러
+    //다음 버튼
     const handleNextClick = async () => {
+
         if (!content?.trim()) {
             alert("덕담을 입력해주세요!");
             return;
@@ -56,18 +72,33 @@ const Letter = () => {
         setCard(selectedCard); // 선택된 카드 이름 저장
 
         try {
-            const { questionId } = await submitCustomQuestion(
-                title,
-                answers,
-                correctAnswer,
-                card,
-                domain,
-                content
-            );
+            if (isModified && questionCustomId) {
+                const response = await postEdit(
+                    title,
+                    answers,
+                    correctAnswer,
+                    card,
+                    domain,
+                    content
+                );
+                console.log(response);
+                alert("문제 수정 완료!");
+            } else {
+                const response = await submitCustomQuestion(
+                    title,
+                    answers,
+                    correctAnswer,
+                    card,
+                    domain,
+                    content
+                );
 
-            console.log("POST 성공:", { questionId });
+                console.log("POST 성공:", response);
+
+            }
             setStep(4);
-            router.push(`/pockets/complete?questionId=${questionId}`);
+            router.push(`/pockets/complete?questionId=${questionCustomId}`);
+
         } catch (error) {
             console.error("POST 요청 실패:", error);
             alert("복주머니를 만드는 중 문제가 발생했습니다. 다시 시도해주세요.");
@@ -77,7 +108,16 @@ const Letter = () => {
     return (
         <div>
             <Header>
-                <BackButton/>
+                <BackButton
+                    onClick={ async () => {
+                        if (questionCustomId) {
+                            buttonBackClick(questionCustomId); // 데이터 가져오기 및 상태 업데이트
+                            history.back();
+                        } else {
+                            history.back(); // 단순 뒤로 가기
+                        }
+                    }}
+                />
             </Header>
             <div className="p-4">
                 <Notice text="새해 덕담을 작성해주세요!"/>
