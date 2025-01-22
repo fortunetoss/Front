a7f9c75cea00ee5cd828d5520ca927183b4d34db
Lee Yuna <leeyuna7689@gmail.com>
2025-01-22 21:23:35 +0900

    chore: 중복된 테일윈드 클래스 제거

--- src/app/pockets/select/page.tsx
+++ src/app/pockets/select/page.tsx
@@ -19,34 +19,34 @@ const Select = () => {
   };
 
   return (
-      <div>
-          <Header>
-              <BackButton/>
-          </Header>
-          <div className="container mx-auto p-6">
+    <div>
+      <Header>
+        <BackButton />
+      </Header>
+      <div className="container mx-auto p-6">
+        <h1 className="font-semibold text-xl mt-1">
+          2024년을 돌아보며
+          <br />
+          문제와 새해 덕담을 전해주세요.
+        </h1>
 
-              <h1 className="font-semibold text-xl mt-1">
-                  2024년을 돌아보며<br/>
-                  문제와 새해 덕담을 전해주세요.
-              </h1>
-
-              <div className="space-y-4 mt-28">
-                  <button
-                      onClick={() => handleOption("problem")}
-                      className="w-full text-black bg-white hover:bg-blue hover:text-white border border-gray-300 text-lg rounded-lg px-4 py-2 shadow hover:bg-blue"
-                  >
-                      문제만 보내기
-                  </button>
-                  <button
-                      onClick={() => handleOption("together")}
-                      className="w-full text-black hover:bg-blue hover:text-white border border-gray-300 text-lg rounded-lg px-4 py-2 shadow hover:bg-blue"
-                  >
-                      문제와 덕담 같이 보내기
-                  </button>
-              </div>
-          </div>
+        <div className="space-y-4 mt-28">
+          <button
+            onClick={() => handleOption("problem")}
+            className="w-full text-black bg-white hover:bg-blue hover:text-white border border-gray-300 text-lg rounded-lg px-4 py-2 shadow"
+          >
+            문제만 보내기
+          </button>
+          <button
+            onClick={() => handleOption("together")}
+            className="w-full text-black hover:bg-blue hover:text-white border border-gray-300 text-lg rounded-lg px-4 py-2 shadow"
+          >
+            문제와 덕담 같이 보내기
+          </button>
+        </div>
       </div>
+    </div>
   );
 };
 
-          export default Select;
+export default Select;
