c87e5115051f652508a5331d3c219cbf0ff8cb35
Lee Yuna <leeyuna7689@gmail.com>
2025-01-24 16:40:54 +0900

    fix: 오류 및 스타일링 수정

--- src/utils/url/useShare.ts
+++ src/utils/url/useShare.ts
@@ -3,42 +3,42 @@ import { generateUrl } from "@/utils/url/urlGenerator";
 import { kakaotalkShare } from "@/utils/share/kakaotalk-share";
 
 const useShareHandlers = (questionCustomId: string | null) => {
-    const [shareableUrl, setShareableUrl] = useState<string | null>(null);
+  const [shareableUrl, setShareableUrl] = useState<string | null>(null);
 
-    useEffect(() => {
-        if (!questionCustomId) return;
-        const url = generateUrl(questionCustomId);
-        if (url) {
-            setShareableUrl(url);
-        } else {
-            console.error("URL 생성 실패");
-        }
-    }, [questionCustomId]);
+  useEffect(() => {
+    if (!questionCustomId) return;
+    const url = generateUrl(Number(questionCustomId));
+    if (url) {
+      setShareableUrl(url);
+    } else {
+      console.error("URL 생성 실패");
+    }
+  }, [questionCustomId]);
 
-    const handleCopyUrl = () => {
-        if (shareableUrl) {
-            navigator.clipboard
-                .writeText(shareableUrl)
-                .then(() => alert("URL이 클립보드에 복사되었습니다!"))
-                .catch((err) => console.error("URL 복사 실패:", err));
-        } else {
-            alert("공유 가능한 URL이 없습니다.");
-        }
-    };
+  const handleCopyUrl = () => {
+    if (shareableUrl) {
+      navigator.clipboard
+        .writeText(shareableUrl)
+        .then(() => alert("URL이 클립보드에 복사되었습니다!"))
+        .catch((err) => console.error("URL 복사 실패:", err));
+    } else {
+      alert("공유 가능한 URL이 없습니다.");
+    }
+  };
 
-    const handleKakaoShare = () => {
-        if (shareableUrl) {
-            kakaotalkShare(shareableUrl);
-        } else {
-            alert("공유 가능한 URL이 없습니다.");
-        }
-    };
+  const handleKakaoShare = () => {
+    if (shareableUrl) {
+      kakaotalkShare(shareableUrl, {});
+    } else {
+      alert("공유 가능한 URL이 없습니다.");
+    }
+  };
 
-    return {
-        shareableUrl,
-        handleCopyUrl,
-        handleKakaoShare,
-    };
+  return {
+    shareableUrl,
+    handleCopyUrl,
+    handleKakaoShare,
+  };
 };
 
 export default useShareHandlers;
