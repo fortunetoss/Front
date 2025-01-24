8a4585ee089ecdf79a4ec1dad9026214028df83c
Lee Yuna <leeyuna7689@gmail.com>
2025-01-24 16:55:49 +0900

    feat: 카카오톡 공유 이미지 설정

--- src/utils/share/kakaotalk-share.ts
+++ src/utils/share/kakaotalk-share.ts
@@ -1,6 +1,8 @@
+import { getPouch } from "../images/domain";
+
 export const kakaotalkShare = (
   targetUrl: string,
-  { isAnswerer = false, hasMessage = false }
+  { isAnswerer = false, hasMessage = false, pouchType = "A" }
 ) => {
   if (!window.Kakao || !window.Kakao.isInitialized()) {
     console.error("Kakao SDK가 초기화되지 않았습니다.");
@@ -9,19 +11,20 @@ export const kakaotalkShare = (
 
   let title = "복 던지미가 보내온 ‘복 주머니’를 받아보세요!";
   let description = "복 던지미가 보내온 퀴즈 풀기!";
-  let imageUrl =
-    "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg";
   let buttonText = "문제 풀러가기";
+  let imageSrc;
 
   if (hasMessage) {
     description += " 🍀퀴즈를 풀면 덕담을 드려요!";
   }
 
   if (isAnswerer) {
-    // 문구 변경하기
     title = "문제에 대한 응답이 도착했어요!";
     description = "응답자가 정답을 맞췄는지 확인해보세요!";
     buttonText = "응답 결과 확인하기";
+    imageSrc = getPouch(pouchType);
+  } else {
+    imageSrc = "/sharingImg.png";
   }
 
   window.Kakao.Share.sendDefault({
@@ -29,7 +32,7 @@ export const kakaotalkShare = (
     content: {
       title,
       description,
-      imageUrl,
+      imageUrl: imageSrc ? `${process.env.NEXT_PUBLIC_URL}${imageSrc}` : null,
       link: {
         mobileWebUrl: targetUrl,
         webUrl: targetUrl,
